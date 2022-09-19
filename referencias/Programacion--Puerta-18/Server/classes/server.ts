fd646c335199343ae2d4fd5b5e3c5bd9c17740eb
AleStringer <112954309+AleStringer@users.noreply.github.com>
2022-09-19 19:38:56 -0300

    fixes server setup

--- referencias/Programacion--Puerta-18/Server/classes/server.ts
+++ referencias/Programacion--Puerta-18/Server/classes/server.ts
@@ -1,11 +1,13 @@
-import express from 'express';
+const express = require("express")
 import { SERVER_PORT } from '../global/environment';
 
 export class Servidor {
+    //@ts-ignore
     public app: express.Application;
     public port: number;
 
     public constructor(){
+        console.log("wewew")
         this.app = express();
         this.port = SERVER_PORT;
     }
--- referencias/Programacion--Puerta-18/Server/index.ts
+++ referencias/Programacion--Puerta-18/Server/index.ts
@@ -1,24 +1,23 @@
-import {Servidor} from "./classes/server";
+import { Servidor } from "./classes/server";
 import { router } from "./routes/router";
-import cors from "cors";
-import bodyParser from "body-parser";
+const cors = require("cors")
+const bodyParser = require("body-parser");
 
 let server = new Servidor();
 
 server.app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }));
 server.app.use(bodyParser.json());
 
-server.app.use(cors({origin:true, credentials:true}));
+server.app.use(cors({ origin: true, credentials: true }));
 // Credenciales de seguridad
 
 server.app.use('/', router)
 
-server.start( () => {
+server.start(() => {
     console.log("servidor corriendo en " + server.port);
-    
 });
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+
 
